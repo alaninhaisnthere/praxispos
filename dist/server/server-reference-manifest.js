b7ed3eaaac103b7ac27acb1358f197982a18eb4f
Alana Oliveira <alanaoliveira71@gmail.com>
2024-05-24 08:35:52 -0300

    feat/landingpage: small fixes (favicon)

--- dist/server/server-reference-manifest.js
+++ dist/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9BHeuYEFFLWNSGHZ2bqISpLeK6F1RZw6hc9rE24PPQE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"REbvJzuHBABVltL/beCeQ78KLnj+lwNkTngwfG+k0c4=\"\n}"
